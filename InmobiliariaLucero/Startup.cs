0cff1384c0bdb8b69ed46938403d881e1d4f332d
valuart <42857715+valuart@users.noreply.github.com>
2021-04-22 20:01:27 -0300

    so many cambios

--- InmobiliariaLucero/Startup.cs
+++ InmobiliariaLucero/Startup.cs
@@ -11,6 +11,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace InmobiliariaLucero
 {
@@ -20,73 +22,72 @@ namespace InmobiliariaLucero
         public Startup(IConfiguration configuration)
         {
             this.configuration = configuration;
-        
+
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
 
-            public void ConfigureServices(IServiceCollection services)
-            {
-                services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                    .AddCookie(options =>//el sitio web valida con cookie
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>//el sitio web valida con cookie
                 {
-                        options.LoginPath = "/Usuario/Login";
-                        options.LogoutPath = "/Usuario/Logout";
-                        options.AccessDeniedPath = "/Home/Restringido";
+                    options.LoginPath = "/Usuario/Login";
+                    options.LogoutPath = "/Usuario/Logout";
+                    options.AccessDeniedPath = "/Home/Restringido";
 
-                    });
+                });
 
             services.AddAuthorization(options =>
             {
                 options.AddPolicy("Administrador", policy => policy.RequireClaim(ClaimTypes.Role, "Administrador", "SuperAdministrador"));
             });
 
-             services.AddControllersWithViews();
+            services.AddControllersWithViews();
 
         }
 
 
-            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
             {
-                if (env.IsDevelopment())
-                {
-                    app.UseDeveloperExceptionPage();
-                }
-                else
-                {
-                    app.UseExceptionHandler("/Home/Error");
-                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                    app.UseHsts();
-                }
-                app.UseHttpsRedirection();
-                app.UseStaticFiles();
-                app.UseRouting();
-                app.UseCookiePolicy(new CookiePolicyOptions
-                {
-                    MinimumSameSitePolicy = SameSiteMode.None,
-                });
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseCookiePolicy(new CookiePolicyOptions
+            {
+                MinimumSameSitePolicy = SameSiteMode.None,
+            });
 
-                app.UseAuthentication();
-                app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-                if (env.IsDevelopment())
-                {
-                    app.UseDeveloperExceptionPage();//página amarilla de errores
-                }
-
-
-                app.UseEndpoints(endpoints =>
-                {
-                    endpoints.MapControllerRoute("login", "login/{**accion}", new { controller = "Usuario", action = "Login" });
-
-                    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-                });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();//página amarilla de errores
             }
 
 
-        }
-    }
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("login", "login/{**accion}", new { controller = "Usuario", action = "Login" });
 
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+
+    }
+}
